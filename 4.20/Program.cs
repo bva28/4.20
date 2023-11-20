85a3b50f1ba5e760c8cfe9e59cda6f6ffd6fa4b3
bva28 <5rkuytsur@gmail.com>
2023-11-20 12:58:07 +0300

    Добавьте файлы проекта.

---
+++ 4.20/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите часы:");
+int hours = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите минуты:");
+int minutes = Convert.ToInt32(Console.ReadLine());
+
+int total;
+
+if ((hours > 0) && (minutes > 0) && (minutes < 59))
+{
+    total = (hours * 60) + minutes;
+    Console.WriteLine($"Время в минутах: {total} ");
+}
+else
+{
+    Console.WriteLine("Значение введено некорректно");
+}
