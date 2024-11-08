17b70f6499009e7d3a202e0600c800366651a345
vab82 <5rkuytsur@gmail.com>
2024-11-08 10:56:23 +0700

    Fix

--- WinFormsApp1/MainForm.Designer.cs
+++ WinFormsApp1/MainForm.Designer.cs
@@ -107,7 +107,7 @@ namespace View
 			dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
 			dataGridViewCellStyle1.BackColor = SystemColors.Window;
 			dataGridViewCellStyle1.Font = new Font("Segoe UI", 9F);
-			dataGridViewCellStyle1.ForeColor = Color.Bisque;
+			dataGridViewCellStyle1.ForeColor = Color.Black;
 			dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
 			dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
 			dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
